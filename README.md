24321ba9e10b208d8b18c8d2d816588f8511f8cb
Jin Kim <jkim1665@gmail.com>
2019-04-30 18:13:19 -0400

    some bugs . needs updates

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # BurgerApp
 This app will store every burger in a database.
+
+In this assignment, you'll create a burger logger with MySQL, Node, Express, Handlebars and a homemade ORM (yum!). Be sure to follow the MVC design pattern; use Node and MySQL to query and route data in your app, and Handlebars to generate your HTML.
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('BaconCheeseBurger');
+INSERT INTO burgers (name) VALUES ('Chicken Sandwhich');
+
+INSERT INTO burgers (name, devoured) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('BaconCheeseBurger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Chicken Sandwhich', true);
